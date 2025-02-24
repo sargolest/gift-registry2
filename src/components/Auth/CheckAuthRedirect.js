6716ee4b83406a55f7d9dfa0fbe288ef487314ed
sargolest <145877635+sargolest@users.noreply.github.com>
2025-02-25 01:34:15 +0330

    Update CheckAuthRedirect.js

--- src/components/Auth/CheckAuthRedirect.js
+++ src/components/Auth/CheckAuthRedirect.js
@@ -1,1 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { signIn, useSession } from "next-auth/react";
+
+export default function CheckAuthRedirect() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/dashboard");
+    }
+  }, [session, status, router]);
+
+  return null;
+}
 
