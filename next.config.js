c633952f2dcbcf53bd77edfdf8d08783ab97dc99
sargolest <145877635+sargolest@users.noreply.github.com>
2025-02-25 18:12:12 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['firebasestorage.googleapis.com'],
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
